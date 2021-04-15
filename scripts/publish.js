2ea0d39f479b6372979edf6af021acac7361f8b5
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2021-04-15 14:06:31 +0200

    add publish script

---
+++ scripts/publish.js
@@ -0,0 +1,85 @@
+#!/usr/bin/env node
+
+const path = require('path');
+
+const spawnAsync = require('@expo/spawn-async');
+
+const lerna = path.join(__dirname, '../node_modules/.bin/lerna');
+
+const shouldPrerelease = isPrerelease();
+
+function isPrerelease() {
+  const bumpFlagIndex = process.argv.findIndex((arg) => arg === '--bump');
+  const prereleaseArgIndex = process.argv.findIndex((arg) => arg === 'prerelease');
+  return (
+    bumpFlagIndex !== -1 && prereleaseArgIndex !== -1 && bumpFlagIndex + 1 === prereleaseArgIndex
+  );
+}
+
+async function run() {
+  const preReleaseFlags = shouldPrerelease ? ['--no-git-tag-version', '--no-push'] : [];
+  await spawnAsync(lerna, ['version', '--exact', ...preReleaseFlags, ...process.argv.slice(2)], {
+    stdio: 'inherit',
+  });
+
+  const packages = JSON.parse(
+    (
+      await spawnAsync(lerna, ['ls', '--toposort', '--json'], {
+        stdio: ['inherit', 'pipe', 'inherit'],
+      })
+    ).stdout
+  );
+
+  console.log('🔎 Looking for packages to publish');
+  const toPublish = [];
+  for (const { name, version, location } of packages) {
+    let packageViewStdout;
+    try {
+      packageViewStdout = (
+        await spawnAsync('npm', ['view', '--json', name], {
+          stdio: ['inherit', 'pipe', 'inherit'],
+        })
+      ).stdout;
+    } catch (e) {
+      const response = JSON.parse(e.stdout);
+      if (response.error && response.error.code === 'E404') {
+        toPublish.push({ name, location });
+        console.log(`* ${name} 🆕`);
+      } else {
+        throw e;
+      }
+      continue;
+    }
+
+    const packageView = JSON.parse(packageViewStdout);
+    if (!packageView.versions.includes(version)) {
+      toPublish.push({ name, location, version });
+      console.log(`* ${name}`);
+    }
+  }
+
+  if (toPublish.length === 0) {
+    console.log('✅ No packages left to publish');
+    return;
+  }
+
+  for (const { name, location } of toPublish) {
+    console.log();
+    console.log('🚢 Publishing', name);
+    const args = ['publish'];
+    if (shouldPrerelease) {
+      args.push('--tag', 'alpha');
+    }
+
+    await spawnAsync('npm', args, {
+      cwd: location,
+      stdio: 'inherit',
+    });
+    console.log('✅ Published', name);
+  }
+}
+
+run().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
