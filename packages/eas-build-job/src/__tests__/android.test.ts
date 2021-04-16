ce7cc2a4474f3685af5c965c313e4ecebefb6f06
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2021-04-16 13:40:30 +0200

    move @expo/eas-build-job to eas-build (#3)
    * copy
    
    * adapt to eas-build repo

---
+++ packages/eas-build-job/src/__tests__/android.test.ts
@@ -0,0 +1,126 @@
+import Joi from '@hapi/joi';
+
+import * as Android from '../android';
+import { ArchiveSourceType, Platform, Workflow } from '../common';
+
+const joiOptions: Joi.ValidationOptions = {
+  stripUnknown: true,
+  convert: true,
+  abortEarly: false,
+};
+
+const secrets = {
+  buildCredentials: {
+    keystore: {
+      dataBase64: 'MjEzNwo=',
+      keystorePassword: 'pass1',
+      keyAlias: 'alias',
+      keyPassword: 'pass2',
+    },
+  },
+};
+
+describe('Android.GenericJobSchema', () => {
+  test('valid job', () => {
+    const genericJob = {
+      secrets,
+      platform: Platform.ANDROID,
+      type: Workflow.GENERIC,
+      projectArchive: {
+        type: ArchiveSourceType.URL,
+        url: 'http://localhost:3000',
+      },
+      gradleCommand: ':app:bundleRelease',
+      artifactPath: 'android/app/build/outputs/bundle/release/app-release.aab',
+      projectRootDirectory: '.',
+      releaseChannel: 'default',
+      builderEnvironment: {
+        image: 'default',
+        node: '1.2.3',
+        yarn: '2.3.4',
+        ndk: '4.5.6',
+        env: {
+          SOME_ENV: '123',
+        },
+      },
+    };
+
+    const { value, error } = Android.GenericJobSchema.validate(genericJob, joiOptions);
+    expect(value).toMatchObject(genericJob);
+    expect(error).toBeFalsy();
+  });
+
+  test('invalid job', () => {
+    const genericJob = {
+      secrets,
+      platform: Platform.ANDROID,
+      type: Workflow.GENERIC,
+      projectArchive: {
+        type: ArchiveSourceType.URL,
+        url: 'url',
+      },
+      gradleCommand: 1,
+      uknownField: 'field',
+      projectRootDirectory: '.',
+    };
+
+    const { value, error } = Android.GenericJobSchema.validate(genericJob, joiOptions);
+    expect(error?.message).toBe(
+      '"projectArchive.url" must be a valid uri. "gradleCommand" must be a string'
+    );
+    expect(value).not.toMatchObject(genericJob);
+  });
+});
+
+describe('Android.ManagedJobSchema', () => {
+  test('valid job', () => {
+    const managedJob = {
+      secrets,
+      platform: Platform.ANDROID,
+      type: Workflow.MANAGED,
+      buildType: Android.ManagedBuildType.APP_BUNDLE,
+      username: 'turtle-tutorial',
+      projectArchive: {
+        type: ArchiveSourceType.URL,
+        url: 'http://localhost:3000',
+      },
+      projectRootDirectory: '.',
+      releaseChannel: 'default',
+      builderEnvironment: {
+        image: 'default',
+        node: '1.2.3',
+        yarn: '2.3.4',
+        ndk: '4.5.6',
+        env: {
+          SOME_ENV: '123',
+        },
+      },
+    };
+
+    const { value, error } = Android.ManagedJobSchema.validate(managedJob, joiOptions);
+    expect(value).toMatchObject(managedJob);
+    expect(error).toBeFalsy();
+  });
+
+  test('invalid job', () => {
+    const managedJob = {
+      secrets,
+      platform: Platform.ANDROID,
+      type: Workflow.MANAGED,
+      buildType: Android.ManagedBuildType.APP_BUNDLE,
+      username: 3,
+      projectArchive: {
+        type: ArchiveSourceType.URL,
+        url: 'url',
+      },
+      projectRootDirectory: '.',
+      uknownField: 'field',
+    };
+
+    const { value, error } = Android.ManagedJobSchema.validate(managedJob, joiOptions);
+    expect(error?.message).toBe(
+      '"projectArchive.url" must be a valid uri. "username" must be a string'
+    );
+    expect(value).not.toMatchObject(managedJob);
+  });
+});
---
+++ packages/eas-build-job/src/__tests__/metadata.test.ts
@@ -0,0 +1,45 @@
+import { MetadataSchema } from '../metadata';
+
+describe('MetadataSchema', () => {
+  test('valid metadata', () => {
+    const metadata = {
+      appName: 'testapp',
+      appVersion: '1.0.0',
+      cliVersion: '1.2.3',
+      buildProfile: 'release',
+      credentialsSource: 'remote',
+      distribution: 'store',
+      gitCommitHash: '752e99d2b8fde1bf07ebb8af1b4a3c26a6703943',
+      trackingContext: {},
+      workflow: 'generic',
+    };
+    const { value, error } = MetadataSchema.validate(metadata, {
+      stripUnknown: true,
+      convert: true,
+      abortEarly: false,
+    });
+    expect(error).toBeFalsy();
+    expect(value).toEqual(metadata);
+  });
+  test('invalid metadata', () => {
+    const metadata = {
+      appName: 'testapp',
+      appVersion: '1.0.0',
+      cliVersion: '1.2.3',
+      buildProfile: 'release',
+      credentialsSource: 'blah',
+      distribution: 'store',
+      gitCommitHash: 'inv4lid-h@sh',
+      trackingContext: {},
+      workflow: 'generic',
+    };
+    const { error } = MetadataSchema.validate(metadata, {
+      stripUnknown: true,
+      convert: true,
+      abortEarly: false,
+    });
+    expect(error?.message).toEqual(
+      '"credentialsSource" must be one of [local, remote]. "gitCommitHash" length must be 40 characters long. "gitCommitHash" must only contain hexadecimal characters'
+    );
+  });
+});
