25f75ec544fdd727e4d57c9aa9d52ef1207a9421
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2021-04-26 11:44:01 +0200

    [local-build-plugin] check environment before build (#10)

--- packages/local-build-plugin/src/main.ts
+++ packages/local-build-plugin/src/main.ts
@@ -3,12 +3,14 @@ import chalk from 'chalk';
 import { parseInputAsync } from './parseInput';
 import { buildAsync } from './build';
 import { listenForInterrupts, shouldExit } from './exit';
+import { checkRuntimeAsync } from './checkRuntime';
 
 listenForInterrupts();
 
 async function main(): Promise<void> {
   try {
     const { job } = await parseInputAsync();
+    await checkRuntimeAsync(job);
     await buildAsync(job);
   } catch (err) {
     if (!shouldExit()) {
