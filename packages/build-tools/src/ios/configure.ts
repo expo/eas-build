20c00a063a5ef79f5d8ff40a5aae1777b9388fe5
Dominik Sokal <dominik.sokal@swmansion.com>
2021-05-26 11:59:55 +0200

    remove backward compatibility for old ios credentials format

--- packages/build-tools/src/ios/configure.ts
+++ packages/build-tools/src/ios/configure.ts
@@ -11,21 +11,15 @@ async function configureXcodeProject<TJob extends Ios.Job>(
 ): Promise<void> {
   ctx.logger.info('Configuring Xcode project');
   const targetNames = Object.keys(credentials.targetProvisioningProfiles);
-  // __eas_build_default_target__ is a temporary name
-  // should match the value in turtle-api
-  const onlyDefaultTarget =
-    targetNames.length === 1 && targetNames[0] === '__eas_build_default_target__';
   for (const targetName of targetNames) {
     const profile = credentials.targetProvisioningProfiles[targetName];
     ctx.logger.info(
-      `Assigning provisioning profile '${profile.name}' (Apple Team ID: ${profile.teamId}) to ${
-        onlyDefaultTarget ? 'the default target' : `target '${targetName}'`
-      }`
+      `Assigning provisioning profile '${profile.name}' (Apple Team ID: ${profile.teamId}) to target '${targetName}'`
     );
     IOSConfig.ProvisioningProfile.setProvisioningProfileForPbxproj(
       ctx.reactNativeProjectDirectory,
       {
-        targetName: onlyDefaultTarget ? undefined : targetName,
+        targetName,
         profileName: profile.name,
         appleTeamId: profile.teamId,
       }
