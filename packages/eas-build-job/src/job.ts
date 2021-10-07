e3e0a372bacedc4505ebb22c4c7c2585bc03072f
Dominik Sokal <dominik.sokal@swmansion.com>
2021-10-07 14:39:42 +0200

    [eas-build-job] set builder image based on sdk version (#48)

--- packages/eas-build-job/src/job.ts
+++ packages/eas-build-job/src/job.ts
@@ -1,6 +1,7 @@
 import Joi from 'joi';
+import semver from 'semver';
 
-import { Platform } from './common';
+import { Platform, Workflow } from './common';
 import * as Android from './android';
 import * as Ios from './ios';
 
@@ -14,15 +15,43 @@ export const JobSchema = Joi.object<Job>({
   .when(Joi.object({ platform: Platform.ANDROID }).unknown(), { then: Android.JobSchema })
   .when(Joi.object({ platform: Platform.IOS }).unknown(), { then: Ios.JobSchema });
 
-export function sanitizeJob(job: object): Job {
-  const { value, error } = JobSchema.validate(job, {
+export function sanitizeJob(rawJob: object, { sdkVersion }: { sdkVersion?: string } = {}): Job {
+  const { value, error } = JobSchema.validate(rawJob, {
     stripUnknown: true,
     convert: true,
     abortEarly: false,
   });
+
+  const job: Job = value;
+  setIosBuilderImageForManagedJob(job, sdkVersion);
+
   if (error) {
     throw error;
   } else {
-    return value;
+    return job;
   }
+}
+
+function setIosBuilderImageForManagedJob(job: Job, sdkVersion?: string): void {
+  if (
+    !(
+      job.platform === Platform.IOS &&
+      job.type === Workflow.MANAGED &&
+      !job.builderEnvironment?.image &&
+      sdkVersion
+    )
+  ) {
+    return;
+  }
+
+  const ranges = Object.keys(Ios.sdkVersionToDefaultBuilderImage);
+  const matchingRange = ranges.find((range) => semver.satisfies(sdkVersion, range));
+  if (!matchingRange) {
+    return;
+  }
+  const image = Ios.sdkVersionToDefaultBuilderImage[matchingRange];
+  job.builderEnvironment = {
+    image,
+    ...job.builderEnvironment,
+  };
 }
