da54e307ea9b201d12d94dd7f38bb7c6ba49c3a3
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2022-01-04 10:52:16 +0100

    [local-builds] specify custom artifact path via env (#66)

--- packages/local-build-plugin/src/config.ts
+++ packages/local-build-plugin/src/config.ts
@@ -10,6 +10,7 @@ const envWorkingdir = process.env.EAS_LOCAL_BUILD_WORKINGDIR;
 const envSkipCleanup = process.env.EAS_LOCAL_BUILD_SKIP_CLEANUP;
 const envSkipNativeBuild = process.env.EAS_LOCAL_BUILD_SKIP_NATIVE_BUILD;
 const envArtifactsDir = process.env.EAS_LOCAL_BUILD_ARTIFACTS_DIR;
+const envArtifactPath = process.env.EAS_LOCAL_BUILD_ARTIFACT_PATH;
 
 if (envLoggerLevel && !['debug', 'info', 'warn', 'error'].includes(envLoggerLevel)) {
   throw new Error(
@@ -22,6 +23,7 @@ export default {
   skipCleanup: envSkipCleanup === '1',
   skipNativeBuild: envSkipNativeBuild === '1',
   artifactsDir: envArtifactsDir ?? process.cwd(),
+  artifactPath: envArtifactPath,
   logger: {
     level: (envLoggerLevel ?? 'info') as 'debug' | 'info' | 'warn' | 'error',
   },
