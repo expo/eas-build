4277fbb00d337f85c34ca45e19aa6e9fe07d9003
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2022-01-13 12:29:56 +0100

    review feedback

--- packages/local-build-plugin/src/eject.ts
+++ packages/local-build-plugin/src/eject.ts
@@ -37,19 +37,23 @@ export class LocalExpoCliEjectProvider implements EjectProvider<Job> {
   private getPrebuildCommand(job: Job): string {
     let prebuildCommand =
       job.experimental?.prebuildCommand ?? `prebuild --non-interactive --platform ${job.platform}`;
-    if (!prebuildCommand.match(/--platform/)) {
+    if (!prebuildCommand.match(/(?:--platform| -p)/)) {
       prebuildCommand = `${prebuildCommand} --platform ${job.platform}`;
     }
     if (!prebuildCommand.match(/--non-interactive/)) {
       prebuildCommand = `${prebuildCommand} --non-interactive`;
     }
+    const npxCommandPrefix = 'npx ';
     const expoCommandPrefix = 'expo ';
     const expoCliCommandPrefix = 'expo-cli ';
+    if (prebuildCommand.startsWith(npxCommandPrefix)) {
+      prebuildCommand = prebuildCommand.substr(npxCommandPrefix.length).trim();
+    }
     if (prebuildCommand.startsWith(expoCommandPrefix)) {
-      prebuildCommand = prebuildCommand.substr(expoCommandPrefix.length);
+      prebuildCommand = prebuildCommand.substr(expoCommandPrefix.length).trim();
     }
     if (prebuildCommand.startsWith(expoCliCommandPrefix)) {
-      prebuildCommand = prebuildCommand.substr(expoCliCommandPrefix.length);
+      prebuildCommand = prebuildCommand.substr(expoCliCommandPrefix.length).trim();
     }
     return prebuildCommand;
   }
