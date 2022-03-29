6152484c6905ff28396e5db3e68c8d58df5363fd
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2022-03-29 11:41:26 +0200

    update fs-extra

--- packages/build-tools/src/__mocks__/fs.ts
+++ packages/build-tools/src/__mocks__/fs.ts
@@ -1,10 +1,11 @@
 import { fs } from 'memfs';
 
-// `temp-dir` dependency of `tempy` is using `fs.realpathSync('/tmp')`
-// on import to verify existence of tmp directory
 fs.mkdirSync('/tmp');
 if (process.env.TMPDIR) {
   fs.mkdirSync(process.env.TMPDIR, { recursive: true });
 }
 
-module.exports = fs;
+const fsRealpath = fs.realpath;
+(fsRealpath as any).native = fsRealpath;
+
+module.exports = { ...fs, realpath: fsRealpath };
