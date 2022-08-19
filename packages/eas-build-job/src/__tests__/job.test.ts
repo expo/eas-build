6288f8c670d7ec96316612c335d7126be6519904
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2022-08-19 12:52:34 +0200

    use test.each

--- packages/eas-build-job/src/__tests__/job.test.ts
+++ packages/eas-build-job/src/__tests__/job.test.ts
@@ -1,42 +1,36 @@
 import { Workflow } from '../common';
 import { setAndroidBuilderImage, setIosBuilderImageForManagedJob } from '../job';
 
-function testAndroidImage(
-  sdkVersion?: string,
-  reactNativeVersion?: string,
-  expectedImage?: string
-): void {
-  const job: any = { builderEnvironment: {} };
-  setAndroidBuilderImage(job, sdkVersion, reactNativeVersion);
-  expect(job?.builderEnvironment?.image).toBe(expectedImage);
-}
-
 describe(setAndroidBuilderImage, () => {
-  test('selecting image', () => {
-    testAndroidImage('45.0.0', '0.69.0', 'ubuntu-18.04-jdk-11-ndk-r19c');
-    testAndroidImage('45.0.0', '0.67.0', 'ubuntu-18.04-jdk-8-ndk-r19c');
-    testAndroidImage('46.0.0', '0.69.0', 'ubuntu-20.04-jdk-11-ndk-r21e');
-    testAndroidImage(undefined, '0.69.0', undefined);
-    testAndroidImage('46.0.0', undefined, undefined);
-  });
+  test.each([
+    ['45.0.0', '0.69.0', 'ubuntu-18.04-jdk-11-ndk-r19c'],
+    ['45.0.0', '0.67.0', 'ubuntu-18.04-jdk-8-ndk-r19c'],
+    ['46.0.0', '0.69.0', 'ubuntu-20.04-jdk-11-ndk-r21e'],
+    [undefined, '0.69.0', undefined],
+    ['46.0.0', undefined, undefined],
+  ])(
+    'selecting image for sdkVersion %s and React Native %s',
+    (sdkVersion?: string, reactNativeVersion?: string, expectedImage?: string) => {
+      const job: any = { builderEnvironment: {} };
+      setAndroidBuilderImage(job, sdkVersion, reactNativeVersion);
+      expect(job?.builderEnvironment?.image).toBe(expectedImage);
+    }
+  );
 });
 
-function testIosImage(
-  sdkVersion: string | undefined,
-  workflow: Workflow,
-  expectedImage: string | undefined
-): void {
-  const job: any = { builderEnvironment: {}, type: workflow };
-  setIosBuilderImageForManagedJob(job, sdkVersion);
-  expect(job?.builderEnvironment?.image).toBe(expectedImage);
-}
-
 describe(setIosBuilderImageForManagedJob, () => {
-  test('selecting image', () => {
-    testIosImage('44.0.0', Workflow.MANAGED, 'macos-big-sur-11.4-xcode-13.0');
-    testIosImage('45.0.0', Workflow.MANAGED, 'macos-monterey-12.3-xcode-13.3');
-    testIosImage('46.0.0', Workflow.MANAGED, 'macos-monterey-12.3-xcode-13.3');
-    testIosImage('46.0.0', Workflow.GENERIC, undefined);
-    testIosImage(undefined, Workflow.MANAGED, undefined);
-  });
+  test.each([
+    ['44.0.0', Workflow.MANAGED, 'macos-big-sur-11.4-xcode-13.0'],
+    ['45.0.0', Workflow.MANAGED, 'macos-monterey-12.3-xcode-13.3'],
+    ['46.0.0', Workflow.MANAGED, 'macos-monterey-12.3-xcode-13.3'],
+    ['46.0.0', Workflow.GENERIC, undefined],
+    [undefined, Workflow.MANAGED, undefined],
+  ])(
+    'selecting image for sdkVersion %s and React Native %s',
+    (sdkVersion: string | undefined, workflow: Workflow, expectedImage: string | undefined) => {
+      const job: any = { builderEnvironment: {}, type: workflow };
+      setIosBuilderImageForManagedJob(job, sdkVersion);
+      expect(job?.builderEnvironment?.image).toBe(expectedImage);
+    }
+  );
 });
