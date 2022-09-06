b6b91f2a83962094d877fb3c50cc4d6766ee5ce1
Szymon Dziedzic <sz.dziedz@gmail.com>
2022-09-06 14:40:34 +0200

    [build-tools] use cocoapods proxy

--- packages/build-tools/src/ios/pod.ts
+++ packages/build-tools/src/ios/pod.ts
@@ -1,5 +1,6 @@
 import path from 'path';
 
+import fs from 'fs-extra';
 import { Ios } from '@expo/eas-build-job';
 import spawn from '@expo/turtle-spawn';
 
@@ -7,6 +8,13 @@ import { BuildContext } from '../context';
 
 export async function installPods<TJob extends Ios.Job>(ctx: BuildContext<TJob>): Promise<void> {
   const iosDir = path.join(ctx.reactNativeProjectDirectory, 'ios');
+
+  if (ctx.env.EAS_BUILD_COCOAPODS_CACHE_URL && Math.random() < 0.33) {
+    ctx.logger.info('Running using experimental Cocoapods cache proxy...');
+    const podFile = path.join(iosDir, 'Podfile');
+    await fs.appendFile(podFile, `\nsource "${ctx.env.EAS_BUILD_COCOAPODS_CACHE_URL}"`);
+  }
+
   ctx.logger.info('Installing pods');
   await spawn('pod', ['install'], {
     cwd: iosDir,
