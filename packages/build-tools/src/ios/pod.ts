05095f648648bb86440384fd3ff52771d0f3bdee
Szymon Dziedzic <sz.dziedz@gmail.com>
2022-09-15 11:37:48 +0200

    [build-tools] use cocoapods plugin to handle setting proxy as a source

--- packages/build-tools/src/ios/pod.ts
+++ packages/build-tools/src/ios/pod.ts
@@ -1,6 +1,5 @@
 import path from 'path';
 
-import fs from 'fs-extra';
 import { Ios } from '@expo/eas-build-job';
 import spawn from '@expo/turtle-spawn';
 
@@ -8,28 +7,6 @@ import { BuildContext } from '../context';
 
 export async function installPods<TJob extends Ios.Job>(ctx: BuildContext<TJob>): Promise<void> {
   const iosDir = path.join(ctx.reactNativeProjectDirectory, 'ios');
-
-  if (ctx.env.EAS_BUILD_COCOAPODS_CACHE_URL) {
-    const podfilePath = path.join(iosDir, 'Podfile');
-    const originalPodfileContents = await fs.readFile(podfilePath, 'utf-8');
-
-    const cocoaPodsCdnSourceRegex = /\bsource\s*\(?['"]https:\/\/cdn\.cocoapods\.org\/?['"]\)?/g;
-
-    if (originalPodfileContents.search(cocoaPodsCdnSourceRegex) !== -1) {
-      await fs.writeFile(
-        podfilePath,
-        originalPodfileContents.replace(
-          cocoaPodsCdnSourceRegex,
-          `source "${ctx.env.EAS_BUILD_COCOAPODS_CACHE_URL}"`
-        )
-      );
-    } else {
-      await fs.writeFile(
-        podfilePath,
-        `source "${ctx.env.EAS_BUILD_COCOAPODS_CACHE_URL}"\n${originalPodfileContents}`
-      );
-    }
-  }
 
   await spawn('pod', ['install'], {
     cwd: iosDir,
