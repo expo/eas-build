2d7e72ef538fea6b2f139c01055bddc3a0f69687
Dominik Sokal <dominik.sokal@swmansion.com>
2022-09-16 12:04:42 +0200

    remove backward compatibility for environment secrets (#146)

--- packages/eas-build-job/src/ios.ts
+++ packages/eas-build-job/src/ios.ts
@@ -91,7 +91,7 @@ export interface Job {
   };
   secrets: {
     buildCredentials?: BuildCredentials;
-    environmentSecrets?: EnvironmentSecret[] | Env;
+    environmentSecrets?: EnvironmentSecret[];
   };
   builderEnvironment?: BuilderEnvironment;
   cache: Cache;
@@ -134,7 +134,7 @@ export const JobSchema = Joi.object({
   }),
   secrets: Joi.object({
     buildCredentials: BuildCredentialsSchema,
-    environmentSecrets: Joi.alternatives().try(EnvironmentSecretsSchema, EnvSchema),
+    environmentSecrets: EnvironmentSecretsSchema,
   }).required(),
   builderEnvironment: BuilderEnvironmentSchema,
   cache: CacheSchema.default(),
