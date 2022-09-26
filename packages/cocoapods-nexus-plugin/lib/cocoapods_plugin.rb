9478cf215efd5e9524d3822a2cc162e5048186d1
Szymon Dziedzic <sz.dziedz@gmail.com>
2022-09-26 19:23:49 +0200

    [cocoapods-nexus-plugin] blacklist MapboxCoreMaps pod

--- packages/cocoapods-nexus-plugin/lib/cocoapods_plugin.rb
+++ packages/cocoapods-nexus-plugin/lib/cocoapods_plugin.rb
@@ -3,7 +3,7 @@ require 'fileutils'
 
 CDN_URL = "https://cdn.cocoapods.org"
 
-POD_BLACKLIST = ["libwebp", "Braintree", "razorpay-pod", "TrezorCrypto", "MapboxCommon"]
+POD_BLACKLIST = ["libwebp", "Braintree", "razorpay-pod", "TrezorCrypto", "MapboxCommon", "MapboxCoreMaps"]
 
 NEXUS_COCOAPODS_REPO_URL = ENV['NEXUS_COCOAPODS_REPO_URL']
 
