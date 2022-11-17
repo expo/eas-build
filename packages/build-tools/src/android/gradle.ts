3d97753cf9a150f09e20967951b9db5c0832e758
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2022-11-17 14:40:06 +0100

    [build-tools] Set oom_score_adj on all gradle processes (#172)

--- packages/build-tools/src/android/gradle.ts
+++ packages/build-tools/src/android/gradle.ts
@@ -1,8 +1,10 @@
 import path from 'path';
+import assert from 'assert';
 
-import spawn from '@expo/turtle-spawn';
+import spawn, { SpawnPromise, SpawnResult } from '@expo/turtle-spawn';
 import { Android, Job } from '@expo/eas-build-job';
 import fs from 'fs-extra';
+import { bunyan } from '@expo/logger';
 
 import { BuildContext } from '../context';
 
@@ -23,7 +25,7 @@ export async function runGradleCommand(
 ): Promise<void> {
   const androidDir = path.join(ctx.reactNativeProjectDirectory, 'android');
   ctx.logger.info(`Running 'gradlew ${gradleCommand}' in ${androidDir}`);
-  await spawn('bash', ['-c', `sh gradlew ${gradleCommand}`], {
+  const spawnPromise = spawn('bash', ['-c', `sh gradlew ${gradleCommand}`], {
     cwd: androidDir,
     logger: ctx.logger,
     lineTransformer: (line?: string) => {
@@ -35,4 +37,58 @@ export async function runGradleCommand(
     },
     env: ctx.env,
   });
+  if (ctx.env.EAS_BUILD_RUNNER === 'eas-build' && process.platform === 'linux') {
+    adjustOOMScore(spawnPromise, ctx.logger);
+  }
+
+  await spawnPromise;
+}
+
+/**
+ * OOM Killer sometimes kills worker server while build is exceeding memory limits.
+ * `oom_score_adj` is a value between -1000 and 1000 and it defaults to 0.
+ * It defines which process is more likely to get killed (higher value more likely).
+ *
+ * This function sets oom_score_adj for Gradle process and all its child processes.
+ */
+function adjustOOMScore(spawnPromise: SpawnPromise<SpawnResult>, logger: bunyan): void {
+  setTimeout(
+    async () => {
+      try {
+        assert(spawnPromise.child.pid);
+        const children: number[] = [spawnPromise.child.pid];
+        let shouldRetry = true;
+        while (shouldRetry) {
+          const result = await spawn('pgrep', ['-P', children.join(',')], {
+            stdio: 'pipe',
+          });
+          const pids = result.stdout
+            .toString()
+            .split('\n')
+            .map((i) => Number(i.trim()))
+            .filter((i) => i);
+          shouldRetry = false;
+          for (const pid of pids) {
+            if (!children.includes(pid)) {
+              shouldRetry = true;
+              children.push(pid);
+            }
+          }
+        }
+        await Promise.all(
+          children.map(async (pid: number) => {
+            // Value 800 is just a guess here. It's probably higher than most other
+            // process. I didn't want to set it any higher, because I'm not sure if OOM Killer
+            // can start killing processes when there is still enough memory left.
+            const oomScoreOverride = 800;
+            await fs.writeFile(`/proc/${pid}/oom_score_adj`, `${oomScoreOverride}\n`);
+          })
+        );
+      } catch (err: any) {
+        logger.debug({ err, stderr: err?.stderr }, 'Failed to override oom_score_adj');
+      }
+    },
+    // Wait 20 seconds to make sure all child processes are started
+    20000
+  );
 }
