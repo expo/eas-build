e5d026fd060f06e0f3fdb8c01ae472eeb927845a
Dominik Sokal <dominik.sokal@swmansion.com>
2023-01-25 14:39:37 +0100

    fix tests

--- packages/build-tools/src/__mocks__/fs.ts
+++ packages/build-tools/src/__mocks__/fs.ts
@@ -8,4 +8,19 @@ if (process.env.TMPDIR) {
 const fsRealpath = fs.realpath;
 (fsRealpath as any).native = fsRealpath;
 
-module.exports = { ...fs, realpath: fsRealpath };
+const fsRm = (
+  path: string,
+  options: object,
+  callback: (err: NodeJS.ErrnoException | null) => void
+): void => {
+  fs.promises
+    .rm(path, options)
+    .then(() => {
+      callback(null);
+    })
+    .catch((err) => {
+      callback(err);
+    });
+};
+
+module.exports = { ...fs, realpath: fsRealpath, rm: fsRm };
