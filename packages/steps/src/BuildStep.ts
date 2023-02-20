3ab1cbf896df38323004eb0385b8140114f5278b
Dominik Sokal <dominik.sokal@swmansion.com>
2023-02-20 14:58:35 +0100

    [steps] add metalogs indicating start and end of build step logs

--- packages/steps/src/BuildStep.ts
+++ packages/steps/src/BuildStep.ts
@@ -22,13 +22,20 @@ export enum BuildStepStatus {
   NEW = 'new',
   IN_PROGRESS = 'in-progress',
   CANCELED = 'canceled',
-  FAILED = 'failed',
-  SUCCEEDED = 'succeeded',
+  FAIL = 'fail',
+  WARNING = 'warning',
+  SUCCESS = 'success',
+}
+
+export enum BuildStepLogMarker {
+  START_STEP = 'start-step',
+  END_STEP = 'end-step',
 }
 
 export class BuildStep {
   public readonly id: string;
   public readonly name?: string;
+  public readonly displayName?: string;
   public readonly inputs?: BuildStepInput[];
   public readonly outputs?: BuildStepOutput[];
   public readonly command: string;
@@ -63,6 +70,7 @@ export class BuildStep {
   ) {
     this.id = id;
     this.name = name;
+    this.displayName = this.getStepDisplayName(name, command);
     this.inputs = inputs;
     this.outputs = outputs;
     this.outputById =
@@ -78,14 +86,21 @@ export class BuildStep {
     this.status = BuildStepStatus.NEW;
 
     this.internalId = uuidv4();
-    this.logger = ctx.logger.child({ buildStepInternalId: this.internalId, buildStepId: this.id });
+    this.logger = ctx.logger.child({
+      buildStepInternalId: this.internalId,
+      buildStepId: this.id,
+      buildStepDisplayName: this.displayName,
+    });
 
     ctx.registerStep(this);
   }
 
   public async executeAsync(): Promise<void> {
     try {
-      this.logger.debug(`Executing build step "${this.id}"`);
+      this.logger.info(
+        { marker: BuildStepLogMarker.START_STEP },
+        `Executing build step "${this.id}"`
+      );
       this.status = BuildStepStatus.IN_PROGRESS;
 
       const command = this.interpolateInputsInCommand(this.command, this.inputs);
@@ -111,10 +126,18 @@ export class BuildStep {
       await this.collectAndValidateOutputsAsync(outputsDir);
       this.logger.debug('Finished collecting output paramters');
 
-      this.logger.debug('Finished successfully');
-      this.status = BuildStepStatus.SUCCEEDED;
+      this.logger.info(
+        { marker: BuildStepLogMarker.END_STEP, result: BuildStepStatus.SUCCESS },
+        `Finished build step "${this.id}" successfully`
+      );
+      this.status = BuildStepStatus.SUCCESS;
     } catch (err) {
-      this.status = BuildStepStatus.FAILED;
+      this.logger.error({ err });
+      this.logger.error(
+        { marker: BuildStepLogMarker.END_STEP, result: BuildStepStatus.FAIL },
+        `Build step "${this.id}" failed`
+      );
+      this.status = BuildStepStatus.FAIL;
       throw err;
     } finally {
       this.executed = true;
@@ -194,5 +217,21 @@ export class BuildStep {
       __EXPO_STEPS_OUTPUTS_DIR: outputsDir,
       PATH: `${BIN_PATH}:${process.env.PATH}`,
     };
+  }
+
+  private getStepDisplayName(name: string | undefined, command: string): string | undefined {
+    if (name) {
+      return name;
+    }
+    if (command !== '') {
+      const splits = command.trim().split('\n');
+      for (const split of splits) {
+        const trimmed = split.trim();
+        if (trimmed && !trimmed.startsWith('#')) {
+          return trimmed;
+        }
+      }
+    }
+    return undefined;
   }
 }
