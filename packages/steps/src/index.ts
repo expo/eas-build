520524d66bc94f9ba9a302ec703b84e09fa056c9
Dominik Sokal <dominik.sokal@swmansion.com>
2023-03-21 11:45:12 +0100

    [steps] remove upload-artifact command

--- packages/steps/src/index.ts
+++ packages/steps/src/index.ts
@@ -1,4 +1,3 @@
-export { BuildArtifactType } from './BuildArtifacts.js';
 export { BuildConfigParser } from './BuildConfigParser.js';
 export { BuildFunction } from './BuildFunction.js';
 export { BuildStepInput } from './BuildStepInput.js';
