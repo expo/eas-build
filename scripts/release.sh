b3fe2262088f8d25ab7035a88a135deb64edc8fe
Dominik Sokal <dominik.sokal@swmansion.com>
2023-03-28 13:17:07 +0200

    update docs

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,5 +8,5 @@ ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"
 LERNA_BUMP="${1:-patch}"
 
 pushd $ROOT_DIR >/dev/null 2>&1
-lerna version --no-push --exact "$LERNA_BUMP"
+lerna version --exact "$LERNA_BUMP"
 popd >/dev/null 2>&1
