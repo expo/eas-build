064c7271bd32d4e6182a1ff59f337f6b652a53c2
Stanisław Chmiela <sjchmiela@gmail.com>
2023-06-02 15:45:14 +0200

    Revert "Add `id` in private `_emit`"
    This reverts commit c5b7a8d5f89a56eab3f2799c30ba97a6e2d6c65d.

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,28 +1,15 @@
 import bunyan from 'bunyan';
-import { v1 } from 'uuid';
 
 import LoggerLevel from './level';
 import { pipe, pipeSpawnOutput, PipeMode } from './pipe';
 
 const DEFAULT_LOGGER_NAME = 'expo-logger';
 
-interface BunyanLogger extends bunyan {
-  _emit: (rec: any, noEmit: boolean) => string;
-}
-
 function createLogger(options: bunyan.LoggerOptions): bunyan {
-  const logger = bunyan.createLogger({
+  return bunyan.createLogger({
     serializers: bunyan.stdSerializers,
     ...options,
-  }) as BunyanLogger;
-
-  const originalEmit = logger._emit.bind(logger);
-  logger._emit = (rec, noEmit) => {
-    rec.id = v1();
-    return originalEmit(rec, noEmit);
-  };
-
-  return logger;
+  });
 }
 
 const defaultLogger = createLogger({
