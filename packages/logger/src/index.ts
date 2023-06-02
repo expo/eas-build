c5b7a8d5f89a56eab3f2799c30ba97a6e2d6c65d
Stanisław Chmiela <sjchmiela@gmail.com>
2023-06-02 15:31:50 +0200

    Add `id` in private `_emit`

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,15 +1,28 @@
 import bunyan from 'bunyan';
+import { v1 } from 'uuid';
 
 import LoggerLevel from './level';
 import { pipe, pipeSpawnOutput, PipeMode } from './pipe';
 
 const DEFAULT_LOGGER_NAME = 'expo-logger';
 
+interface BunyanLogger extends bunyan {
+  _emit: (rec: any, noEmit: boolean) => string;
+}
+
 function createLogger(options: bunyan.LoggerOptions): bunyan {
-  return bunyan.createLogger({
+  const logger = bunyan.createLogger({
     serializers: bunyan.stdSerializers,
     ...options,
-  });
+  }) as BunyanLogger;
+
+  const originalEmit = logger._emit.bind(logger);
+  logger._emit = (rec, noEmit) => {
+    rec.id = v1();
+    return originalEmit(rec, noEmit);
+  };
+
+  return logger;
 }
 
 const defaultLogger = createLogger({
