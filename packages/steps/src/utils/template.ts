088f1677f0ce40581f7098267c89ecf2023ba6f3
Szymon Dziedzic <szymon.dziedzic@swmansion.com>
2023-06-22 10:13:57 +0200

    use lodash.get

--- packages/steps/src/utils/template.ts
+++ packages/steps/src/utils/template.ts
@@ -1,4 +1,4 @@
-import _ from 'lodash';
+import get from 'lodash.get';
 
 import { BuildStepInputValueTypeName } from '../BuildStepInput.js';
 import { BuildConfigError, BuildStepRuntimeError } from '../errors.js';
@@ -24,23 +24,22 @@ export function interpolateWithOutputs(
 }
 
 export function getObjectValueForInterpolation(
-  path: string,
+  pathWithObjectName: string,
   obj: Record<string, any>
-): string | number | boolean | undefined | null {
-  const arrPath = _.toPath(path.split('.').slice(1).join('.'));
+): string | number | boolean | null {
+  const value = get(obj, pathWithObjectName.split('.').slice(1).join('.'));
 
-  let value = obj as any;
-  for (const key of arrPath) {
-    if (typeof value !== 'object' || (typeof value === 'object' && !(key in value))) {
-      throw new BuildStepRuntimeError(
-        `Object field "${path}" does not exist. Ensure you are using the correct field name.`
-      );
-    }
-    value = value[key];
+  if (value === undefined) {
+    throw new BuildStepRuntimeError(
+      `Object field "${pathWithObjectName}" does not exist. Ensure you are using the correct field name.`
+    );
   }
+
   if (!isAllowedValueTypeForObjectInterpolation(value)) {
     throw new BuildStepRuntimeError(
-      `EAS context field "${path}" is not of type ${Object.values(BuildStepInputValueTypeName).join(
+      `EAS context field "${pathWithObjectName}" is not of type ${Object.values(
+        BuildStepInputValueTypeName
+      ).join(
         ', '
       )}, or undefined. It is of type "${typeof value}". We currently only support accessing string or undefined values from the EAS context.`
     );
@@ -80,10 +79,9 @@ function interpolate(
 
 function isAllowedValueTypeForObjectInterpolation(
   value: any
-): value is string | undefined | number | boolean | object | null {
+): value is string | number | boolean | object | null {
   return (
     typeof value === 'string' ||
-    typeof value === 'undefined' ||
     typeof value === 'number' ||
     typeof value === 'boolean' ||
     typeof value === 'object' ||
@@ -105,11 +103,11 @@ export function findOutputPaths(templateString: string): BuildOutputPath[] {
   return result;
 }
 
-export function parseOutputPath(outputPath: string): BuildOutputPath {
-  const splits = outputPath.split('.').slice(1);
+export function parseOutputPath(outputPathWithObjectName: string): BuildOutputPath {
+  const splits = outputPathWithObjectName.split('.').slice(1);
   if (splits.length !== 2) {
     throw new BuildConfigError(
-      `Step output path must consist of two components joined with a dot, where first is the step ID, and second is the output name, e.g. "step3.output1". Passed: "${outputPath}"`
+      `Step output path must consist of two components joined with a dot, where first is the step ID, and second is the output name, e.g. "step3.output1". Passed: "${outputPathWithObjectName}"`
     );
   }
   const [stepId, outputId] = splits;
