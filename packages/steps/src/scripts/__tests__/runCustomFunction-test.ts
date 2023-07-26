79d66ee5007aaf21698dcf7b040514dfc78ce096
Szymon Dziedzic <szymon.dziedzic@swmansion.com>
2023-07-26 19:45:04 +0200

    improve tests

--- packages/steps/src/scripts/__tests__/runCustomFunction-test.ts
+++ packages/steps/src/scripts/__tests__/runCustomFunction-test.ts
@@ -5,17 +5,20 @@ import { createContext } from 'this-file';
 import { BuildStepInput, BuildStepInputValueTypeName } from '../../BuildStepInput.js';
 import { BuildStepOutput } from '../../BuildStepOutput.js';
 import { createStepContextMock } from '../../__tests__/utils/context.js';
-import { spawnAsync } from '../../utils/shell/spawn.js';
 import {
   cleanUpStepTemporaryDirectoriesAsync,
   createTemporaryEnvsDirectoryAsync,
   createTemporaryOutputsDirectoryAsync,
 } from '../../BuildTemporaryFiles.js';
 import { BIN_PATH } from '../../utils/shell/bin.js';
+import { createCustomFunctionCall } from '../../utils/customFunction.js';
 
 describe('runCustomFunction', () => {
   test('can run custom function', async () => {
-    const ctx = createStepContextMock();
+    const dirname = createContext().dirname;
+    const ctx = createStepContextMock({
+      workingDirectory: path.resolve(dirname, '../../__tests__/fixtures'),
+    });
     const outputs = {
       name: new BuildStepOutput(ctx.global, {
         id: 'name',
@@ -57,33 +60,25 @@ describe('runCustomFunction', () => {
     inputs.num.set(123);
     inputs.obj.set({ foo: 'bar' });
 
-    const dirname = createContext().dirname;
-
     try {
       const outputsDir = await createTemporaryOutputsDirectoryAsync(ctx.global, 'test');
       const envsDir = await createTemporaryEnvsDirectoryAsync(ctx.global, 'test');
 
       const currentPath = process.env.PATH;
       const newPath = currentPath ? `${BIN_PATH}:${currentPath}` : BIN_PATH;
-      const promise = spawnAsync('node', [
-        path.resolve(dirname, '../../../dist_commonjs/scripts/runCustomFunction.cjs'),
-        path.resolve(dirname, '../../__tests__/fixtures/my-custom-ts-function'),
-        JSON.stringify({
-          env: {
-            __EXPO_STEPS_OUTPUTS_DIR: outputsDir,
-            __EXPO_STEPS_ENVS_DIR: envsDir,
-            __EXPO_STEPS_WORKING_DIRECTORY: ctx.workingDirectory,
-            PATH: newPath,
-          },
-          inputs: Object.fromEntries(
-            Object.entries(inputs).map(([id, input]) => [id, input.serialize()])
-          ),
-          outputs: Object.fromEntries(
-            Object.entries(outputs).map(([id, output]) => [id, output.serialize()])
-          ),
-          ctx: ctx.serialize(),
-        }),
-      ]);
+      const fn = createCustomFunctionCall(
+        path.resolve(dirname, '../../__tests__/fixtures/my-custom-ts-function')
+      );
+      const promise = fn(ctx, {
+        env: {
+          __EXPO_STEPS_OUTPUTS_DIR: outputsDir,
+          __EXPO_STEPS_ENVS_DIR: envsDir,
+          __EXPO_STEPS_WORKING_DIRECTORY: ctx.workingDirectory,
+          PATH: newPath,
+        },
+        inputs,
+        outputs,
+      });
       await expect(promise).resolves.not.toThrow();
     } finally {
       await cleanUpStepTemporaryDirectoriesAsync(ctx.global, 'test');
