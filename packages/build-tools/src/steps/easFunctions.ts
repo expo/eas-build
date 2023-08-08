186168336678193249febece4dcec4578cac3d22
Szymon Dziedzic <szymon.dziedzic@swmansion.com>
2023-08-08 14:13:55 +0200

    [build-tools] add `utils/run_gradle` function (#266)

--- packages/build-tools/src/steps/easFunctions.ts
+++ packages/build-tools/src/steps/easFunctions.ts
@@ -14,6 +14,7 @@ import { createFindAndUploadBuildArtifactsBuildFunction } from './functions/eas/
 import { configureEASUpdateIfInstalledFunction } from './functions/eas/configureExpoUpdatesIfInstalled';
 import { injectAndroidCredentialsFunction } from './functions/utils/injectAndroidCredentials';
 import { configureAndroidVersionFunction } from './functions/utils/configureAndroidVersion';
+import { runGradleFunction } from './functions/utils/runGradle';
 
 export function getEasFunctions(
   ctx: CustomBuildContext,
@@ -30,5 +31,6 @@ export function getEasFunctions(
     configureEASUpdateIfInstalledFunction(),
     injectAndroidCredentialsFunction(),
     configureAndroidVersionFunction(),
+    runGradleFunction(),
   ];
 }
---
+++ packages/build-tools/src/steps/functions/utils/runGradle.ts
@@ -0,0 +1,34 @@
+import path from 'path';
+import assert from 'assert';
+
+import { BuildFunction, BuildStepInput, BuildStepInputValueTypeName } from '@expo/steps';
+
+import { resolveGradleCommand, runGradleCommand } from '../../utils/android/gradle';
+
+export function runGradleFunction(): BuildFunction {
+  return new BuildFunction({
+    namespace: 'utils',
+    id: 'run_gradle',
+    name: 'Run gradle',
+    inputProviders: [
+      BuildStepInput.createProvider({
+        id: 'command',
+        required: false,
+        allowedValueTypeName: BuildStepInputValueTypeName.STRING,
+      }),
+    ],
+    fn: async (stepCtx, { env, inputs }) => {
+      assert(stepCtx.global.staticContext.job, 'Job is required');
+      const command = resolveGradleCommand(
+        stepCtx.global.staticContext.job,
+        inputs.command.value as string | undefined
+      );
+      await runGradleCommand({
+        logger: stepCtx.logger,
+        gradleCommand: command,
+        androidDir: path.join(stepCtx.workingDirectory, 'android'),
+        env,
+      });
+    },
+  });
+}
