e03c853e2112721f1d35ec0acfad6c2a57a1dc40
Piotr Szeremeta <piotrek.szeremeta@swmansion.com>
2023-09-01 11:43:44 +0200

    Export PackageManager enum
    BuildContext contains `packageManager` field, but it can't be used outsidee of build-tools context without this export.

--- packages/build-tools/src/index.ts
+++ packages/build-tools/src/index.ts
@@ -11,6 +11,8 @@ export {
   SkipNativeBuildError,
 } from './context';
 
+export { PackageManager } from './utils/packageManager';
+
 export { findAndUploadXcodeBuildLogsAsync } from './ios/xcodeBuildLogs';
 
 export { Hook, runHookIfPresent } from './utils/hooks';
