2a4be78510df1e59386ed1b9f51641cd5e1132cb
Stanisław Chmiela <sjchmiela@gmail.com>
2024-01-25 16:24:52 +0100

    Add `uploadArtifact` test

---
+++ packages/build-tools/src/__tests__/utils/logger.ts
@@ -0,0 +1,12 @@
+import { bunyan } from '@expo/logger';
+
+export function createMockLogger(): bunyan {
+  const logger = {
+    info: jest.fn(),
+    debug: jest.fn(),
+    error: jest.fn(),
+    warn: jest.fn(),
+    child: jest.fn().mockImplementation(() => createMockLogger()),
+  } as unknown as bunyan;
+  return logger;
+}
