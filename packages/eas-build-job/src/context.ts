22136af2dfa6b85758e3226b46ec857e23cdba4f
Stanisław Chmiela <sjchmiela@gmail.com>
2024-02-14 15:05:08 +0100

    Remove generic

--- packages/eas-build-job/src/context.ts
+++ packages/eas-build-job/src/context.ts
@@ -2,8 +2,8 @@ import { Env } from './common';
 import { Job } from './job';
 import { Metadata } from './metadata';
 
-export type BuildStaticContext<TJob extends Job = Job> = {
-  job: TJob;
+export type BuildStaticContext = {
+  job: Job;
   metadata: Metadata | null;
   env: Env;
 };
