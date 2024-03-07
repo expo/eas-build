206b98e7067415bb01279fba394d48ffde03f85c
Radoslaw Krzemien <radoslaw.krzemien@swmansion.com>
2024-03-07 11:38:20 +0100

    [build-tools] Fix text messages
    Added test to check if the multiline message gets sent properly
    
    See: https://linear.app/expo/issue/ENG-11196/create-custom-build-example-slacking-team-members

--- packages/build-tools/src/steps/functions/__tests__/sendSlackMessage.test.ts
+++ packages/build-tools/src/steps/functions/__tests__/sendSlackMessage.test.ts
@@ -54,6 +54,33 @@ describe(createSendSlackMessageFunction, () => {
     expect(loggerWarnMock).not.toHaveBeenCalled();
   });
 
+  it('calls the webhook when using multiline plain text input', async () => {
+    fetchMock.mockImplementation(() => Promise.resolve({ status: 200, ok: true } as Response));
+    const buildStep = sendSlackMessage.createBuildStepFromFunctionCall(
+      createGlobalContextMock({}),
+      {
+        callInputs: {
+          message: 'Line 1\nLine 2\n\nLine 3',
+        },
+        env: {
+          SLACK_HOOK_URL: 'https://slack.hook.url',
+        },
+        id: sendSlackMessage.id,
+      }
+    );
+    mockLogger(buildStep.ctx.logger);
+    await buildStep.executeAsync();
+    expect(fetchMock).toHaveBeenCalledWith('https://slack.hook.url', {
+      method: 'POST',
+      body: '{"text":"Line 1\nLine 2\n\nLine 3"}',
+      headers: { 'Content-Type': 'application/json' },
+    });
+    expect(loggerInfoMock).toHaveBeenCalledTimes(4);
+    expect(loggerInfoMock).toHaveBeenCalledWith('Sending Slack message');
+    expect(loggerInfoMock).toHaveBeenCalledWith('Slack message sent successfully');
+    expect(loggerWarnMock).not.toHaveBeenCalled();
+  });
+
   it('calls the webhook when using the payload input', async () => {
     fetchMock.mockImplementation(() => Promise.resolve({ status: 200, ok: true } as Response));
     const buildStep = sendSlackMessage.createBuildStepFromFunctionCall(
