811cfb0256415757121bdaa4531bca0452cbca6b
Stanisław Chmiela <sjchmiela@gmail.com>
2024-03-11 22:16:40 +0100

    Add tests

---
+++ packages/build-tools/src/__mocks__/fs/promises.ts
@@ -0,0 +1,15 @@
+import { fs } from 'memfs';
+
+fs.mkdirSync('/tmp', { recursive: true });
+if (process.env.TMPDIR) {
+  fs.mkdirSync(process.env.TMPDIR, { recursive: true });
+}
+
+const fsRealpath = fs.realpath;
+(fsRealpath as any).native = fsRealpath;
+
+const fsRm = (path: string, options: object): Promise<void> => {
+  return fs.promises.rm(path, options);
+};
+
+module.exports = { ...fs.promises, realpath: fsRealpath, rm: fsRm };
---
+++ packages/build-tools/src/steps/functions/__tests__/findAndUploadBuildArtifacts.test.ts
@@ -0,0 +1,91 @@
+import { ManagedArtifactType } from '@expo/eas-build-job';
+import { vol } from 'memfs';
+
+import { createGlobalContextMock } from '../../../__tests__/utils/context';
+import { createTestIosJob } from '../../../__tests__/utils/job';
+import { createMockLogger } from '../../../__tests__/utils/logger';
+import { BuildContext } from '../../../context';
+import { CustomBuildContext } from '../../../customBuildContext';
+import { createFindAndUploadBuildArtifactsBuildFunction } from '../findAndUploadBuildArtifacts';
+
+jest.mock('fs');
+
+describe(createFindAndUploadBuildArtifactsBuildFunction, () => {
+  const contextUploadArtifact = jest.fn();
+  const ctx = new BuildContext(createTestIosJob({}), {
+    env: {},
+    logBuffer: { getLogs: () => [], getPhaseLogs: () => [] },
+    logger: createMockLogger(),
+    uploadArtifact: contextUploadArtifact,
+    workingdir: '',
+    runGlobalExpoCliCommand: jest.fn(),
+  });
+  const customContext = new CustomBuildContext(ctx);
+  const findAndUploadBuildArtifacts = createFindAndUploadBuildArtifactsBuildFunction(customContext);
+
+  it('throws first error', async () => {
+    const globalContext = createGlobalContextMock({});
+    const buildStep = findAndUploadBuildArtifacts.createBuildStepFromFunctionCall(
+      globalContext,
+      {}
+    );
+
+    await expect(buildStep.executeAsync()).rejects.toThrow('There are no files matching pattern');
+  });
+
+  it('does not throw error if it uploads artifact', async () => {
+    const globalContext = createGlobalContextMock({});
+    vol.fromJSON(
+      {
+        'ios/build/test.ipa': '',
+      },
+      globalContext.defaultWorkingDirectory
+    );
+    const buildStep = findAndUploadBuildArtifacts.createBuildStepFromFunctionCall(
+      globalContext,
+      {}
+    );
+
+    await expect(buildStep.executeAsync()).resolves.not.toThrow();
+  });
+
+  it('throws build artifacts error', async () => {
+    const globalContext = createGlobalContextMock({});
+    ctx.job.buildArtifactPaths = ['worker.log'];
+    vol.fromJSON(
+      {
+        'ios/build/test.ipa': '',
+      },
+      globalContext.defaultWorkingDirectory
+    );
+    const buildStep = findAndUploadBuildArtifacts.createBuildStepFromFunctionCall(
+      globalContext,
+      {}
+    );
+
+    await expect(buildStep.executeAsync()).rejects.toThrow('No such file or directory worker.log');
+  });
+
+  it('upload build artifacts and throws application archive error', async () => {
+    const globalContext = createGlobalContextMock({});
+    ctx.job.buildArtifactPaths = ['worker.log'];
+    vol.fromJSON(
+      {
+        'worker.log': '',
+      },
+      globalContext.defaultWorkingDirectory
+    );
+    const buildStep = findAndUploadBuildArtifacts.createBuildStepFromFunctionCall(
+      globalContext,
+      {}
+    );
+    await expect(buildStep.executeAsync()).rejects.toThrow('There are no files matching pattern');
+    expect(contextUploadArtifact).toHaveBeenCalledWith(
+      expect.objectContaining({
+        artifact: expect.objectContaining({
+          type: ManagedArtifactType.BUILD_ARTIFACTS,
+        }),
+      })
+    );
+  });
+});
