f30b5ba1fc0eb5dbe409379578e78a79c8eab819
Stanisław Chmiela <sjchmiela@gmail.com>
2024-03-28 17:02:15 +0100

    Export runCustomBuildAsync

--- packages/build-tools/src/builders/index.ts
+++ packages/build-tools/src/builders/index.ts
@@ -1,4 +1,5 @@
 import androidBuilder from './android';
 import iosBuilder from './ios';
+import { runCustomBuildAsync as genericBuilder } from './custom';
 
-export { androidBuilder, iosBuilder };
+export { androidBuilder, iosBuilder, genericBuilder };
