384c2f6bee9f5152cf6c18f001ae741e3bf93916
Radoslaw Krzemien <radoslaw.krzemien@swmansion.com>
2024-04-26 14:43:55 +0200

    [build-tools] Update tests
    Updated tests to check for successful retries on DNS errors
    
    See: https://linear.app/expo/issue/ENG-11883/retry-uploading-artifacts-to-gcs-in-turtle-workers

--- packages/build-tools/src/__tests__/context.test.ts
+++ packages/build-tools/src/__tests__/context.test.ts
@@ -7,6 +7,17 @@ import { createTestIosJob } from './utils/job';
 
 const mockLogger = createLogger({ name: 'mock-logger' });
 
+class DNSError extends Error {
+  private readonly _code: string = 'ENOTFOUND';
+  constructor(message: string, code?: string) {
+    super(message);
+    this._code = code ?? this._code;
+  }
+  public get code(): string {
+    return this._code;
+  }
+}
+
 describe('uploadArtifact', () => {
   it('uploads artifact if successful first time', async () => {
     const job = createTestIosJob();
@@ -26,7 +37,7 @@ describe('uploadArtifact', () => {
     await ctx.uploadArtifact({ artifact, logger: mockLogger });
     expect(ctx.artifacts[artifact.type]).toEqual('bucketKey');
   });
-  it('uploads artifact if fails first time and then succeeds', async () => {
+  it('uploads artifact if fails first time with DNS error and then succeeds', async () => {
     const job = createTestIosJob();
     const ctx = new BuildContext(job, {
       workingdir: '/workingdir',
@@ -37,7 +48,7 @@ describe('uploadArtifact', () => {
       uploadArtifact: jest
         .fn()
         .mockImplementationOnce(() => {
-          throw new Error('Upload failed once');
+          throw new DNSError('Upload failed once');
         })
         .mockImplementation(() => Promise.resolve('bucketKey')),
     });
@@ -49,7 +60,7 @@ describe('uploadArtifact', () => {
     await ctx.uploadArtifact({ artifact, logger: mockLogger });
     expect(ctx.artifacts[artifact.type]).toEqual('bucketKey');
   });
-  it('uploads artifact if fails twice and then succeeds', async () => {
+  it('uploads artifact if fails twice with DNS error and then succeeds', async () => {
     const job = createTestIosJob();
     const ctx = new BuildContext(job, {
       workingdir: '/workingdir',
@@ -60,10 +71,10 @@ describe('uploadArtifact', () => {
       uploadArtifact: jest
         .fn()
         .mockImplementationOnce(() => {
-          throw new Error('Upload failed once');
+          throw new DNSError('Upload failed once');
         })
         .mockImplementationOnce(() => {
-          throw new Error('Upload failed twice');
+          throw new DNSError('Upload failed twice', 'EAI_AGAIN');
         })
         .mockImplementation(() => Promise.resolve('bucketKey')),
     });
@@ -75,9 +86,9 @@ describe('uploadArtifact', () => {
     await ctx.uploadArtifact({ artifact, logger: mockLogger });
     expect(ctx.artifacts[artifact.type]).toEqual('bucketKey');
   });
-  it('does not upload artifact if fails three times', async () => {
+  it('does not upload artifact if fails three times with DNS error', async () => {
     const job = createTestIosJob();
-    const finalError = new Error('Upload failed thrice');
+    const finalError = new DNSError('Upload failed thrice');
     const ctx = new BuildContext(job, {
       workingdir: '/workingdir',
       logBuffer: { getLogs: () => [], getPhaseLogs: () => [] },
@@ -87,10 +98,10 @@ describe('uploadArtifact', () => {
       uploadArtifact: jest
         .fn()
         .mockImplementationOnce(() => {
-          throw new Error('Upload failed once');
+          throw new DNSError('Upload failed once');
         })
         .mockImplementationOnce(() => {
-          throw new Error('Upload failed twice');
+          throw new DNSError('Upload failed twice');
         })
         .mockImplementationOnce(() => {
           throw finalError;
