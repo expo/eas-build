1c4a7ffc5aa07ac2949e1e0695999ea7958922f4
Radosław Krzemień <radoslaw.krzemien@swmansion.com>
2024-08-22 13:23:12 +0200

    [eas-build] [steps] [ENG-13151] Catch errors while evaluating if step should be executed (#436)
    * [build-tools] [steps] Catch errors when evaluating if step should be executed
    
    Wrapped the function determining if the step should be executed in a try/catch block. The caught error is logged, as is a message that the runner failed to evaluate if the step should be executed or not, so the user knows what's going on. The step is then skipped and the build is failed
    
    See: https://linear.app/expo/issue/ENG-13151/commented-out-lines-in-message-template-fail-the-send-slack-message
    
    * Update packages/steps/src/BuildWorkflow.ts
    
    Co-authored-by: Szymon Dziedzic <szymon.dziedzic@swmansion.com>
    
    ---------
    
    Co-authored-by: Szymon Dziedzic <szymon.dziedzic@swmansion.com>

--- packages/steps/src/BuildWorkflow.ts
+++ packages/steps/src/BuildWorkflow.ts
@@ -19,7 +19,18 @@ export class BuildWorkflow {
     let maybeError: Error | null = null;
     let hasAnyPreviousStepFailed = false;
     for (const step of this.buildSteps) {
-      if (step.shouldExecuteStep(hasAnyPreviousStepFailed)) {
+      let shouldExecuteStep = false;
+      try {
+        shouldExecuteStep = step.shouldExecuteStep(hasAnyPreviousStepFailed);
+      } catch (err: any) {
+        step.ctx.logger.error({ err });
+        step.ctx.logger.error(
+          `Runner failed to evaluate if it should execute step "${step.displayName}", using step's if condition "${step.ifCondition}". This can be caused by trying to access non-existing object property. If you think this is a bug report it here: https://github.com/expo/eas-cli/issues.`
+        );
+        maybeError = maybeError ?? err;
+        hasAnyPreviousStepFailed = true;
+      }
+      if (shouldExecuteStep) {
         try {
           await step.executeAsync();
         } catch (err: any) {
