3740d27452aeb787d292d8656523f96cc71212d4
Stanisław Chmiela <sjchmiela@gmail.com>
2024-11-14 16:35:38 +0100

    Add test

---
+++ packages/build-tools/src/__tests__/context.test.ts
@@ -0,0 +1,81 @@
+import { randomUUID } from 'crypto';
+
+import { BuildTrigger, Job, Metadata } from '@expo/eas-build-job';
+import { vol } from 'memfs';
+
+import { BuildContext } from '../context';
+
+import { createMockLogger } from './utils/logger';
+
+jest.mock('fs');
+jest.mock('fs-extra');
+
+describe('BuildContext', () => {
+  it('should merge secrets', async () => {
+    const robotAccessToken = randomUUID();
+    await vol.promises.mkdir('/workingdir/environment-secrets/', { recursive: true });
+
+    const ctx = new BuildContext(
+      {
+        triggeredBy: BuildTrigger.GIT_BASED_INTEGRATION,
+        secrets: {
+          robotAccessToken,
+          environmentSecrets: [
+            {
+              name: 'TEST_SECRET',
+              value: 'test-secret-value',
+            },
+          ],
+        },
+      } as Job,
+      {
+        env: {},
+        workingdir: '/workingdir',
+        logger: createMockLogger(),
+        logBuffer: { getLogs: () => [], getPhaseLogs: () => [] },
+        uploadArtifact: jest.fn(),
+      }
+    );
+
+    ctx.updateJobInformation({} as Job, {} as Metadata);
+
+    expect(ctx.job.secrets).toEqual({
+      robotAccessToken,
+      environmentSecrets: [
+        {
+          name: 'TEST_SECRET',
+          value: 'test-secret-value',
+        },
+      ],
+    });
+
+    const newRobotAccessToken = randomUUID();
+    ctx.updateJobInformation(
+      {
+        secrets: {
+          robotAccessToken: newRobotAccessToken,
+          environmentSecrets: [
+            {
+              name: 'TEST_SECRET',
+              value: 'new-test-secret-value',
+            },
+            {
+              name: 'TEST_SECRET_2',
+              value: 'test-secret-value-2',
+            },
+          ],
+        },
+      } as Job,
+      {} as Metadata
+    );
+
+    expect(ctx.job.secrets).toEqual({
+      robotAccessToken: newRobotAccessToken,
+      environmentSecrets: [
+        { name: 'TEST_SECRET', value: 'test-secret-value' },
+        { name: 'TEST_SECRET', value: 'new-test-secret-value' },
+        { name: 'TEST_SECRET_2', value: 'test-secret-value-2' },
+      ],
+    });
+  });
+});
