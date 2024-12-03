1dd48a7f68d1f26b31668cdd5370fb584060a623
Will Schurman <wschurman@gmail.com>
2024-12-02 17:50:59 -0800

    [build-tools] Remove fallback unversioned fingerprint diff

--- packages/build-tools/src/utils/diffFingerprintsAsync.ts
+++ packages/build-tools/src/utils/diffFingerprintsAsync.ts
@@ -1,5 +1,4 @@
 import { BuildStepEnv } from '@expo/steps';
-import fs from 'fs-extra';
 import { bunyan } from '@expo/logger';
 
 import {
@@ -9,58 +8,35 @@ import {
   expoFingerprintCommandAsync,
   isModernExpoFingerprintCLISupportedAsync,
 } from './expoFingerprintCli';
-import { diffFingerprints } from './fingerprint';
 
 export async function diffFingerprintsAsync(
   projectDir: string,
   fingerprint1File: string,
   fingerprint2File: string,
   { env, logger }: { env: BuildStepEnv; logger: bunyan }
-): Promise<string> {
+): Promise<string | null> {
   if (!(await isModernExpoFingerprintCLISupportedAsync(projectDir))) {
-    logger.debug('Falling back to local fingerprint diff');
-    return await diffFingerprintsFallbackAsync(fingerprint1File, fingerprint2File);
+    logger.debug('Fingerprint diff not available');
+    return null;
   }
 
   try {
-    return await diffFingerprintsCommandAsync(projectDir, fingerprint1File, fingerprint2File, {
-      env,
-    });
+    return await expoFingerprintCommandAsync(
+      projectDir,
+      ['fingerprint:diff', fingerprint1File, fingerprint2File],
+      {
+        env,
+      }
+    );
   } catch (e) {
     if (
       e instanceof ExpoFingerprintCLIModuleNotFoundError ||
       e instanceof ExpoFingerprintCLICommandFailedError ||
       e instanceof ExpoFingerprintCLIInvalidCommandError
     ) {
-      logger.debug('Falling back to local fingerprint diff');
-      return await diffFingerprintsFallbackAsync(fingerprint1File, fingerprint2File);
+      logger.debug('Fingerprint diff not available');
+      return null;
     }
     throw e;
   }
-}
-
-async function diffFingerprintsCommandAsync(
-  projectDir: string,
-  fingerprint1File: string,
-  fingerprint2File: string,
-  { env }: { env: BuildStepEnv }
-): Promise<string> {
-  return await expoFingerprintCommandAsync(
-    projectDir,
-    ['fingerprint:diff', fingerprint1File, fingerprint2File],
-    {
-      env,
-    }
-  );
-}
-
-async function diffFingerprintsFallbackAsync(
-  fingerprint1File: string,
-  fingerprint2File: string
-): Promise<string> {
-  const [fingeprint1, fingerprint2] = await Promise.all([
-    fs.readJSON(fingerprint1File),
-    fs.readJSON(fingerprint2File),
-  ]);
-  return JSON.stringify(diffFingerprints(fingeprint1, fingerprint2));
 }
