93db1a16685639df31225a747b6161eb09a997d2
Szymon Dziedzic <szymon.dziedzic@swmansion.com>
2025-01-09 14:34:52 +0100

    [build-tools] run pre-install hook with npm when yarn is detected as package manager

--- packages/build-tools/src/utils/hooks.ts
+++ packages/build-tools/src/utils/hooks.ts
@@ -4,7 +4,7 @@ import spawn from '@expo/turtle-spawn';
 import { BuildContext } from '../context';
 
 import { PackageManager } from './packageManager';
-import { isUsingYarn2, readPackageJson } from './project';
+import { readPackageJson } from './project';
 
 export enum Hook {
   PRE_INSTALL = 'eas-build-pre-install',
@@ -28,10 +28,11 @@ export async function runHookIfPresent<TJob extends BuildJob>(
   const packageJson = readPackageJson(projectDir);
   if (packageJson.scripts?.[hook]) {
     ctx.logger.info(`Script '${hook}' is present in package.json, running it...`);
-    // when using yarn 2, it's not possible to run any scripts before running 'yarn install'
-    // use 'npm' in that case
+    // both yarn v2+ and yarn v1 seem to have issues with running preinstall script in some cases
+    // like doing corepack enable
+    // https://exponent-internal.slack.com/archives/C9PRD479V/p1736426668589209
     const packageManager =
-      (await isUsingYarn2(projectDir)) && hook === Hook.PRE_INSTALL
+      ctx.packageManager === PackageManager.YARN && hook === Hook.PRE_INSTALL
         ? PackageManager.NPM
         : ctx.packageManager;
     await spawn(packageManager, ['run', hook], {
