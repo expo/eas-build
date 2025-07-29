2c15c54936c54c725e4ce2e29cea50a4419b168b
Stanisław Chmiela <sjchmiela@gmail.com>
2025-07-29 22:39:11 +0200

    Move

--- packages/build-tools/src/steps/easFunctions.ts
+++ packages/build-tools/src/steps/easFunctions.ts
@@ -30,7 +30,7 @@ import { createSubmissionEntityFunction } from './functions/createSubmissionEnti
 import { createDownloadBuildFunction } from './functions/downloadBuild';
 import { createRepackBuildFunction } from './functions/repack';
 import { createDownloadArtifactFunction } from './functions/downloadArtifact';
-import { createInternalEasMaestroTestFunction } from './functions/maestroTest';
+import { createInternalEasMaestroTestFunction } from './functions/internalMaestroTest';
 
 export function getEasFunctions(ctx: CustomBuildContext): BuildFunction[] {
   const functions = [
