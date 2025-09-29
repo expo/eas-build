6933399efa2dde69b3d7fc22ebe49a5b4d9ad29b
Stanisław Chmiela <sjchmiela@gmail.com>
2025-09-29 15:25:17 +0200

    Remove fingerprint info from job's metadata

--- packages/eas-build-job/src/__tests__/metadata.test.ts
+++ packages/eas-build-job/src/__tests__/metadata.test.ts
@@ -1,4 +1,4 @@
-import { FingerprintSourceType, Metadata, MetadataSchema } from '../metadata';
+import { Metadata, MetadataSchema } from '../metadata';
 
 const validMetadata: Metadata = {
   appName: 'testapp',
@@ -70,58 +70,5 @@ describe('MetadataSchema', () => {
     expect(error?.message).toEqual(
       '"credentialsSource" must be one of [local, remote]. "gitCommitHash" length must be 40 characters long. "gitCommitHash" must only contain hexadecimal characters. "gitCommitMessage" length must be less than or equal to 4096 characters long. "message" length must be less than or equal to 1024 characters long'
     );
-  });
-
-  test('Allows correct fingerprint', () => {
-    const metadata: Metadata = {
-      ...validMetadata,
-      fingerprintSource: {
-        type: FingerprintSourceType.GCS,
-        bucketKey:
-          'development/8a9c5554-cfbe-4b4c-814c-c476a1047db9/fd6f8af4-7293-46bd-bec7-3fe639f4fd3e',
-        isDebugFingerprint: true,
-      },
-    };
-    const { value, error } = MetadataSchema.validate(metadata, {
-      stripUnknown: true,
-      convert: true,
-      abortEarly: false,
-    });
-    expect(error).toBeFalsy();
-    expect(value).toEqual(metadata);
-  });
-
-  test('Validates incorrect fingerprint type', () => {
-    const metadata: Metadata = {
-      ...validMetadata,
-      fingerprintSource: {
-        type: 'BOO' as any,
-        bucketKey:
-          'development/8a9c5554-cfbe-4b4c-814c-c476a1047db9/fd6f8af4-7293-46bd-bec7-3fe639f4fd3e',
-      },
-    };
-    const { error } = MetadataSchema.validate(metadata, {
-      stripUnknown: true,
-      convert: true,
-      abortEarly: false,
-    });
-    expect(error?.message).toEqual('"fingerprintSource.type" must be one of [GCS, PATH, URL]');
-  });
-
-  test('Validates incorrect fingerprint key', () => {
-    const metadata: Metadata = {
-      ...validMetadata,
-      fingerprintSource: {
-        type: FingerprintSourceType.GCS,
-        // @ts-expect-error TypeScript is too smart. Need to ignore this for the failing test
-        url: 'development/8a9c5554-cfbe-4b4c-814c-c476a1047db9/fd6f8af4-7293-46bd-bec7-3fe639f4fd3e',
-      },
-    };
-    const { error } = MetadataSchema.validate(metadata, {
-      stripUnknown: true,
-      convert: true,
-      abortEarly: false,
-    });
-    expect(error?.message).toEqual('"fingerprintSource.bucketKey" is required');
   });
 });
