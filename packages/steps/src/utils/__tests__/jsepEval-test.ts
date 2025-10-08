63e17ab0fb0ee604df4c9e5f54041809849bf705
Stanisław Chmiela <sjchmiela@gmail.com>
2025-10-08 21:23:17 +0200

    Add tests

--- packages/steps/src/utils/__tests__/jsepEval-test.ts
+++ packages/steps/src/utils/__tests__/jsepEval-test.ts
@@ -1,4 +1,14 @@
-import { jsepEval } from '../jsepEval.js';
+import { createHash } from 'node:crypto';
+
+import { jsepEvalAsync } from '../jsepEval.js';
+
+const hashStringAsync = async (...value: string[]): Promise<string> => {
+  const digest = createHash('sha256');
+  for (const v of value) {
+    digest.update(v);
+  }
+  return digest.digest('hex');
+};
 
 const TEST_CASES = [
   ['1 + 1', 2],
@@ -30,12 +40,27 @@ const TEST_CASES = [
   ['fromJSON("{\\"a\\": 1}").a', 1, { fromJSON: JSON.parse }],
   ['fromJSON("{\\"a\\": 1}")[fromJSON(\'"a"\')]', 1, { fromJSON: JSON.parse }],
   ['fromJSON(null).a', undefined, { fromJSON: JSON.parse }],
+  [
+    'hashString(["abc"][0])',
+    'ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad',
+    { hashString: hashStringAsync },
+  ],
+  [
+    'env.PACKAGER == "bun" ? hashString("package.json", "bun.lockb") : hashString("package.json", "package-lock.json")',
+    '43265b153af268a1a2fa679535c0dfdb6c4a2225b65380ff866ee91920ca13df',
+    { hashString: hashStringAsync, env: { PACKAGER: 'bun' } },
+  ],
+  [
+    'hashString("abc") + hashString("def")',
+    'ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015adcb8379ac2098aa165029e3938a51da0bcecfc008fd6795f401178647f96c5b34',
+    { hashString: hashStringAsync },
+  ],
 ] as const;
 
-describe(jsepEval, () => {
-  it('works', () => {
+describe(jsepEvalAsync, () => {
+  it('works', async () => {
     for (const [expr, expectation, context] of TEST_CASES) {
-      expect(jsepEval(expr, context)).toBe(expectation);
+      expect(await jsepEvalAsync(expr, context)).toBe(expectation);
     }
   });
 });
