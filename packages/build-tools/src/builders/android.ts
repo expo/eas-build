4e5c09d4fac5e25f604ce55f796e74e50b3e49d7
Mustafa Abban <mustafa@expo.io>
2025-10-20 15:50:00 -0400

    Update packages/build-tools/src/builders/android.ts
    Co-authored-by: Stanisław Chmiela <sjchmiela@users.noreply.github.com>

--- packages/build-tools/src/builders/android.ts
+++ packages/build-tools/src/builders/android.ts
@@ -121,7 +121,7 @@ async function buildAsync(ctx: BuildContext<Android.Job>): Promise<void> {
             stdio: 'pipe',
           })
         );
-      } catch (err: any) {
+      } catch (err: unknown) {
         if (err instanceof TurtleFetchError && err.response.status === 404) {
           ctx.logger.info('No cache found for this key. Create a cache with function save_cache');
         } else {
