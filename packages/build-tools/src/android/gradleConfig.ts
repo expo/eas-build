b7cf179476e0e07f8f4002ea9b6c141d443c2563
Stanisław Chmiela <sjchmiela@gmail.com>
2025-12-01 14:21:13 +0100

    Update Android build files to use templateString
    Replace file copying and templateFile() calls with templateString()
    and direct string writes. Import template strings from the new
    TypeScript template modules.
    
    Changes:
    - android/gradleConfig.ts: Use EasBuildGradle string
    - steps/utils/android/gradleConfig.ts: Use template strings for
      credentials and version configuration Gradle files

--- packages/build-tools/src/android/gradleConfig.ts
+++ packages/build-tools/src/android/gradleConfig.ts
@@ -5,8 +5,7 @@ import { Android } from '@expo/eas-build-job';
 import fs from 'fs-extra';
 
 import { BuildContext } from '../context';
-
-const EAS_BUILD_GRADLE_TEMPLATE_PATH = path.join(__dirname, '../../templates/eas-build.gradle');
+import { EasBuildGradle } from '../../templates/eas-build.gradle';
 const APPLY_EAS_BUILD_GRADLE_LINE = 'apply from: "./eas-build.gradle"';
 
 export async function configureBuildGradle(ctx: BuildContext<Android.Job>): Promise<void> {
@@ -31,7 +30,7 @@ function getEasBuildGradlePath(projectRoot: string): string {
 
 async function createEasBuildGradle(projectRoot: string): Promise<void> {
   const easBuildGradlePath = getEasBuildGradlePath(projectRoot);
-  await fs.copy(EAS_BUILD_GRADLE_TEMPLATE_PATH, easBuildGradlePath);
+  await fs.writeFile(easBuildGradlePath, EasBuildGradle);
 }
 
 async function addApplyToBuildGradle(projectRoot: string): Promise<void> {
