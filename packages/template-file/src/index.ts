7603bce384ccc8d2e0ec55bf41a026e605aa778d
Stanisław Chmiela <sjchmiela@gmail.com>
2025-12-01 13:22:09 +0100

    Add `templateString`, remove `fs-extra`

--- packages/template-file/src/index.ts
+++ packages/template-file/src/index.ts
@@ -1,3 +1,39 @@
-import templateFile from './templateFile';
+import fs from 'fs/promises';
 
-export = templateFile;
+import lodashTemplate from 'lodash/template';
+
+export function templateString({
+  input,
+  vars,
+  mustache = true,
+}: {
+  input: string;
+  vars: Record<string, unknown>;
+  mustache?: boolean;
+}): string {
+  const compiledTemplate = lodashTemplate(
+    input,
+    mustache
+      ? {
+          interpolate: /{{([\s\S]+?)}}/g,
+        }
+      : undefined
+  );
+  return compiledTemplate(vars);
+}
+
+export async function templateFile(
+  templateFilePath: string,
+  vars: Record<string, unknown>,
+  outputFilePath?: string,
+  options: { mustache?: boolean } = {}
+): Promise<string | void> {
+  const templateContent = await fs.readFile(templateFilePath, 'utf8');
+  const outputFileContents = templateString({ input: templateContent, vars, ...options });
+
+  if (outputFilePath) {
+    await fs.writeFile(outputFilePath, outputFileContents);
+  } else {
+    return outputFileContents;
+  }
+}
