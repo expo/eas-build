b2a679674d419203d431cf03d6654bac70df37f7
Stanisław Chmiela <sjchmiela@gmail.com>
2025-12-10 14:35:28 +0100

    refactor(steps): simplify getDefaultShell as all platforms use bash

--- packages/steps/src/utils/shell/command.ts
+++ packages/steps/src/utils/shell/command.ts
@@ -1,18 +1,8 @@
-import os from 'os';
-
 // stolen from https://circleci.com/docs/configuration-reference/
 const DEFAULT_SHELL = '/bin/bash -eo pipefail';
-const SHELL_BY_PLATFORM: Partial<Record<NodeJS.Platform, string>> = {
-  darwin: '/bin/bash -eo pipefail',
-};
 
-export function getDefaultShell(platform: NodeJS.Platform = os.platform()): string {
-  const platformSpecificShell = SHELL_BY_PLATFORM[platform];
-  if (platformSpecificShell) {
-    return platformSpecificShell;
-  } else {
-    return DEFAULT_SHELL;
-  }
+export function getDefaultShell(): string {
+  return DEFAULT_SHELL;
 }
 
 export function getShellCommandAndArgs(
