bf594a59d5694b00dba00ab5e2bd9ce418cc0202
Stanisław Chmiela <sjchmiela@gmail.com>
2025-12-10 14:34:22 +0100

    fix(steps): remove --login from macos shell to prevent PATH reordering

--- packages/steps/src/utils/shell/command.ts
+++ packages/steps/src/utils/shell/command.ts
@@ -3,7 +3,7 @@ import os from 'os';
 // stolen from https://circleci.com/docs/configuration-reference/
 const DEFAULT_SHELL = '/bin/bash -eo pipefail';
 const SHELL_BY_PLATFORM: Partial<Record<NodeJS.Platform, string>> = {
-  darwin: '/bin/bash --login -eo pipefail',
+  darwin: '/bin/bash -eo pipefail',
 };
 
 export function getDefaultShell(platform: NodeJS.Platform = os.platform()): string {
